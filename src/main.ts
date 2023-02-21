0473ee61ca38bd9203572fdce8d68baff08a593a
S A Saharukh <sasaharukh@gmail.com>
2023-02-22 01:56:53 +0600

    postman added

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .addTag('TODO')
     .addBearerAuth()
+    .setExternalDoc('Postman Collection', '/docs-json')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
