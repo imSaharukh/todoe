9152d760f78fec21f262118b60c264c3cd011ecf
S A Saharukh <sasaharukh@gmail.com>
2023-02-21 12:23:38 +0600

    todo schema added

--- src/todo/entities/todo.entity.ts
+++ src/todo/entities/todo.entity.ts
@@ -1,1 +1,18 @@
-export class Todo {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type TodoDocument = HydratedDocument<Todo>;
+
+@Schema()
+export class Todo {
+  @Prop({ required: true })
+  title: string;
+
+  @Prop({ required: true })
+  description: string;
+
+  @Prop({ required: true, default: false })
+  isDone: boolean;
+}
+
+export const TodoSchema = SchemaFactory.createForClass(Todo);
